a6304eeb43096a78e07d15ccd1bc1f66568469a5
Salim <salimp2009@gmail.com>
2020-02-23 01:49:11 +0300

    updating the shader; incomplete

--- OpenGLCourseApp/main.cpp
+++ OpenGLCourseApp/main.cpp
@@ -19,6 +19,7 @@ void CreateTriangle()
 		 0.0f, 1.0f, 0.0f
 	};
 
+	// trying to sync git
 	// create VAO variable to store the vertex info
 	// 1 stands for the count of variables we are creating; it can be more than one
 	glGenVertexArrays(1, &VAO);			// creating a vertex array generate VAO ID
@@ -35,7 +36,6 @@ void CreateTriangle()
 	glBindVertexArray(0);
 
 }
-
 
 int main() {
 
